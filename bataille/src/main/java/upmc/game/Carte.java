7b560152cf57543c1250dc64d8b2638e334efd35
NicolasB <boullet.nicolas@gmail.com>
2017-09-13 23:51:26 +0200

    premier commit

---
+++ bataille/src/main/java/upmc/game/Carte.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package upmc.game;
+
+/**
+ * @author licence
+ */
+public class Carte {
+    //--
+    // Attributes
+    //--
+    private int num;
+    private String couleur;
+}

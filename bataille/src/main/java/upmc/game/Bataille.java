c49fb85b448f64350d9c0c6f16454637d122d48a
thenear95 <nicolas.sabak@gmail.com>
2017-10-02 16:29:38 +0200

    Test

--- bataille/src/main/java/upmc/game/Bataille.java
+++ bataille/src/main/java/upmc/game/Bataille.java
@@ -18,14 +18,26 @@ import java.util.Scanner;
 
 public class Bataille
 {
-  public static void main(String[] args)
-  {
-    System.out.println("C'est le jeu de la bataille!\n");
-    System.out.print("Veuillez entrer un choix : ");
-    System.out.flush();
-    Scanner console = new Scanner(System.in);
-    String choice = console.nextLine();
-    System.out.println("\nL'utilisateur a choisi : \"" + choice + "\"");
-    System.out.println("C'est terminé :-(\n\nIl n'y a pas beaucoup de fonctionnalités...");
-  }
+
+    private ArrayList<Carte> carteJouees = new ArrayList<Carte>();
+
+    public static void main(String[] args)
+    {
+  /*
+        System.out.println("C'est le jeu de la bataille!\n");
+        System.out.print("Veuillez entrer un choix : ");
+        System.out.flush();
+        Scanner console = new Scanner(System.in);
+        String choice = console.nextLine();
+        System.out.println("\nL'utilisateur a choisi : \"" + choice + "\"");
+        System.out.println("C'est terminé :-(\n\nIl n'y a pas beaucoup de fonctionnalités...");
+        */
+        Joueur joueur = new Joueur();
+
+        while(joueur.getNbCartes() < 54)
+          joueur.pioche();
+
+        joueur.affCartes();
+    }
 }
+
--- bataille/src/main/java/upmc/game/Joueur.java
+++ bataille/src/main/java/upmc/game/Joueur.java
@@ -1,31 +1,31 @@
-// Copyright 2017 Pierre Talbot (IRCAM)
-
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-
-//     http://www.apache.org/licenses/LICENSE-2.0
-
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package upmc.game;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
 
-public class Bataille
-{
-  public static void main(String[] args)
-  {
-    System.out.println("C'est le jeu de la bataille!\n");
-    System.out.print("Veuillez entrer un choix : ");
-    System.out.flush();
-    Scanner console = new Scanner(System.in);
-    String choice = console.nextLine();
-    System.out.println("\nL'utilisateur a choisi : \"" + choice + "\"");
-    System.out.println("C'est terminé :-(\n\nIl n'y a pas beaucoup de fonctionnalités...");
-  }
+/**
+ *
+ * @author licence
+ */
+public class Joueur {
+    
+    private ArrayList<Carte> cartes = new ArrayList<Carte>();
+
+    public Joueur() {
+        
+    }
+    
+    public void pioche() {
+        this.cartes.add(new Carte());
+    }
+
+    public void affCartes() {
+        for (Carte carte: this.cartes) {
+            System.out.println(carte.getValeur() + ", " + carte.getForme());
+        }
+    }
+
+    public int getNbCartes() {
+        return this.cartes.size();
+    }
 }

c7c34fe235689d1e39790eefc8462b5a3f302c39
Adrien Lemaire <ladrien2@gmail.com>
2017-10-09 16:48:17 +0200

    Ajout des fichier oublié

---
+++ bataille/src/main/java/upmc/game/Carte.java
@@ -0,0 +1,38 @@
+package upmc.game;
+
+/**
+ * Created by Adrien on 12/09/2017.
+ */
+public class Carte {
+
+    static String[] couleurs = {"pique","trefle","coeur","carreau"};
+    private String couleur;
+    private int nombre;
+
+    public Carte(String couleur, int nombre) {
+        this.couleur = couleur;
+        this.nombre = nombre;
+    }
+
+    public int compareA(Carte c) {
+        return this.nombre - c.getNombre();
+    }
+
+    private String nomValeur() {
+        String nomValeur;
+        if(this.nombre == 11) nomValeur="valet";
+        else if(this.nombre == 12) nomValeur="reine";
+        else if(this.nombre ==13) nomValeur="roi";
+        else nomValeur = String.valueOf(this.nombre);
+        return nomValeur;
+    }
+
+    public int getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public String toString() {
+        return this.nomValeur()+" de "+this.couleur;
+    }
+}

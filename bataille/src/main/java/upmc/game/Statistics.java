a7f1f7005828906e157de5e1c7f02d9b75713051
timothe <timothe.pardieu@outlook.com>
2017-10-11 12:36:59 +0200

    Utilisation de la méthode permettant de créer et couper un deck dans la classe Bataille (classe mère) afin de mieux profiter de l'héritage. Gestion d'un bug qui compter plusieurs fois des points (à cause de l'appel récursif dans la classe Bataille principale).

--- bataille/src/main/java/upmc/game/Statistics.java
+++ bataille/src/main/java/upmc/game/Statistics.java
@@ -6,16 +6,17 @@ package upmc.game;
  * It can be triggered by selecting the 3rd choice in the menu
  * It support the auto-run (IA vs IA) so we can use it to do some statistics
  * The program will invite you to enter how much runs you want and will display all the steps and the final result
- * (usage : test with 3 or 4 battles then with 20 runs to show that the winning rate will go to the 50%, demonstrate the randomness)
+ * (usage : test with 3 or 4 battles then with 10 runs to show that the winning rate will go to the 50%, demonstrate the randomness)
  * It extends the main WarGame because it's a "type of WarGame". We can use some inheriting from the WarGame class.
  * It can also be used as a unit-test in a way or to play a very quick game
+ *
  * @author Timothé Pardieu
  */
 public class Statistics extends WarGame
 {
 
-    Player j1 = new Player();
-    Player j2 = new Player();
+    private Player j1 = new Player();
+    private Player j2 = new Player();
 
     /**
      * Statistics is a type of WarGame
@@ -23,7 +24,7 @@ public class Statistics extends WarGame
      * @param j1 the first player
      * @param j2 the second one
      */
-    public Statistics(Player j1, Player j2)
+    protected Statistics(Player j1, Player j2)
     {
         super();
         this.j1 = j1;
@@ -32,9 +33,10 @@ public class Statistics extends WarGame
 
     /**
      * run the test (based on warGame)
+     *
      * @param numberOfIteration number of games we need to achieve
      */
-    public void runStat(int numberOfIteration)
+    protected void runStat(int numberOfIteration)
     {
         for (int i = 1; i < numberOfIteration + 1; i++)
         {
@@ -48,9 +50,9 @@ public class Statistics extends WarGame
                 playGame(j1, j2);
             }
         }
-        System.out.println("Score final : Ordi2 => " + j1.getScore() + " vs Ordi1 => " + j2.getScore());
+        System.out.println("Score final : Ordi1 => " + j1.getScore() + " vs Ordi2 => " + j2.getScore());
         System.out.println(j1.getName() + " a gagné " + String.format("%1$,.2f", j1.percentageScore(numberOfIteration)) + " % des matchs");
-        System.out.println(j1.getName() + " a gagné " + String.format("%1$,.2f", j2.percentageScore(numberOfIteration)) + " % des matchs");
+        System.out.println(j2.getName() + " a gagné " + String.format("%1$,.2f", j2.percentageScore(numberOfIteration)) + " % des matchs");
         WarUtility.isQuittingGame("2");
     }
 }

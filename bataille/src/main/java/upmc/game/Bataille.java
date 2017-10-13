dd5de9e5fd755e0968e1eb62452dcdc882ffacde
NicolasB <boullet.nicolas@gmail.com>
2017-10-13 20:54:25 +0200

    division en fonction de la majeure partie du main

--- bataille/src/main/java/upmc/game/Bataille.java
+++ bataille/src/main/java/upmc/game/Bataille.java
@@ -29,9 +29,6 @@ public class Bataille {
         int modeJeu = 0;
         int resultatCombat = 0;
         boolean bataille = false;
-        MenuPseudo menu = new MenuPseudo();
-        LecturePseudo lecturePseudo;
-        ArrayList<String> pseudos;
         Deck deckTempo = new Deck(false);
         Deck deck = new Deck(true);
         Joueur joueur1 = new Joueur("Default1");
@@ -40,35 +37,16 @@ public class Bataille {
         Carte carte2;
 
         //Initialisation du jeu
-        System.out.println("Jeu de la bataille!\n");
-        System.out.println("Le jeu se joue en tour durant lesquels les deux joueurs jouent leurs cartes en meme temps.");
-        System.out.println("Quand un des deux joueurs n'a plus de cartes ou se rend, le jeu s'arrete.");
-        deck.melange();
+        msgInitJeuEtMelange(deck);
 
         //Choix du mode de jeu
-        while(modeJeu != 1 && modeJeu != 2 && modeJeu != 3) {
-            System.out.println("\nChoisissez votre mode de jeu parmis les 2 suivants :");
-            System.out.println(" 1. Deux joueurs\n 2. Contre IA\n 3. Mode rapide contre IA");
-            System.out.println("Votre choix : ");
-            modeJeu = scan.nextInt();
-        }
-        scan.nextLine(); //vide la ligne
-
+        modeJeu = choixModeJeu(modeJeu);
+        
         //Choix du nom des joueurs
-        lecturePseudo = menu.modeLecturePseudo();
-        if(modeJeu == 1)
-            pseudos = lecturePseudo.lirePseudo(false); //Sans IA
-        else
-            pseudos = lecturePseudo.lirePseudo(true); //Avec IA
-        attribuePseudo(joueur1, joueur2, pseudos);
+        choixPseudoJoueur(joueur1, joueur2, modeJeu);
         
         //Distribution égale des cartes entre les deux joueurs
-        while(!deck.estVide()) {
-            if(deck.nbCartes()%2==0)
-                joueur1.ajouterCarte(deck.derniereCarte());
-            else
-                joueur2.ajouterCarte(deck.derniereCarte());
-        }
+        distributionCartes(joueur1, joueur2, deck);
 
         //Boucle de jeu principale
         while(!joueur1.deckVide() && !joueur2.deckVide() && !joueur1.boolAbandon() && !joueur2.boolAbandon()) {
@@ -86,46 +64,42 @@ public class Bataille {
                 resultatCombat = carte1.compare(carte2);
 
                 //Si joueur 1 gagne le combat
-                if(resultatCombat == 1) {
-                    System.out.println(joueur1.afficheNom()+" gagne le combat\n");
-                    joueur1.gagnePoint();
-                    //Le joueur 1 gagne les cartes
-                    while(!deckTempo.estVide())
-                        joueur1.ajouterCarte(deckTempo.derniereCarte());
-
-                    bataille = false;
-                }
-                //Si joueur 2 gagne le combat
-                else if(resultatCombat == -1) {
-                    System.out.println(joueur2.afficheNom()+" gagne le combat\n");
-                    joueur2.gagnePoint();
-                    //Le joueur 2 gagne les cartes
-                    while(!deckTempo.estVide())
-                        joueur2.ajouterCarte(deckTempo.derniereCarte());
-
-                    bataille = false;
-                }
-                //Si bataille
-                else {
-                    System.out.println("Bataille !\n");
-                    //Les deux joueurs posent une carte en plus et on revient au debut de la boucle
-                    //avec cette fois plus de cartes dans le deck tempo jusqu'a qu'un joueur gagne
-                    if(!joueur1.deckVide())
-                        deckTempo.ajouterCarte(joueur1.tireCarte());
-                    if(!joueur2.deckVide())
-                        deckTempo.ajouterCarte(joueur2.tireCarte());
-                    bataille = true;
+                switch (resultatCombat) {
+                    case 1:
+                        System.out.println(joueur1.afficheNom()+" gagne le combat\n");
+                        joueur1.gagnePoint();
+                        //Le joueur 1 gagne les cartes
+                        while(!deckTempo.estVide())
+                            joueur1.ajouterCarte(deckTempo.derniereCarte());
+                        bataille = false;
+                        break;
+                    case -1:
+                        System.out.println(joueur2.afficheNom()+" gagne le combat\n");
+                        joueur2.gagnePoint();
+                        //Le joueur 2 gagne les cartes
+                        while(!deckTempo.estVide())
+                            joueur2.ajouterCarte(deckTempo.derniereCarte());
+                        bataille = false;
+                        break;
+                    default:
+                        System.out.println("Bataille !\n");
+                        //Les deux joueurs posent une carte en plus et on revient au debut de la boucle
+                        //avec cette fois plus de cartes dans le deck tempo jusqu'a qu'un joueur gagne
+                        if(!joueur1.deckVide())
+                            deckTempo.ajouterCarte(joueur1.tireCarte());
+                        if(!joueur2.deckVide())
+                            deckTempo.ajouterCarte(joueur2.tireCarte());
+                        bataille = true;
+                        break;
                 }
 
                 //si bataille on passe cette etape
                 if(!bataille) {
-                    //Demande si les joueurs veulent continuer ou abandonner
-                    System.out.println("Chaque joueur peut maintenant quoi faire :");
-                    System.out.println(" - entree : continuer\n - q+entree : abandonner\n - s+entree : afficher ses stats");
-                    choixJoueur(joueur1, joueur2);
+                    //Demande si les joueurs veulent continuer, abandonner ou afficher des stats
+                    choixSuiteJoueur(joueur1, joueur2);
                     //Si c'est un mode de jeu a 2 joueurs
-                    if(modeJeu == 1) {
-                        choixJoueur(joueur2, joueur1);
+                    if(modeJeu == 1 && !joueur1.boolAbandon()) {
+                        choixSuiteJoueur(joueur2, joueur1);
                     }
                 }
             }
@@ -134,28 +108,28 @@ public class Bataille {
                 //Comparaison
                 resultatCombat = carte1.compare(carte2);
                 //Si joueur 1 gagne le combat
-                if(resultatCombat == 1) {
-                    joueur1.gagnePoint();
-                    //Le joueur 1 gagne les cartes
-                    while(!deckTempo.estVide())
-                        joueur1.ajouterCarte(deckTempo.derniereCarte());
-                    bataille = false;
-                }
-                //Si joueur 2 gagne le combat
-                else if(resultatCombat == -1) {
-                    joueur2.gagnePoint();
-                    //Le joueur 2 gagne les cartes
-                    while(!deckTempo.estVide())
-                        joueur2.ajouterCarte(deckTempo.derniereCarte());
-                    bataille = false;
-                }
-                //Si bataille
-                else {
-                    if(!joueur1.deckVide())
-                        deckTempo.ajouterCarte(joueur1.tireCarte());
-                    if(!joueur2.deckVide())
-                        deckTempo.ajouterCarte(joueur2.tireCarte());
-                    bataille = true;
+                switch (resultatCombat) {
+                    case 1:
+                        joueur1.gagnePoint();
+                        //Le joueur 1 gagne les cartes
+                        while(!deckTempo.estVide())
+                            joueur1.ajouterCarte(deckTempo.derniereCarte());
+                        bataille = false;
+                        break;
+                    case -1:
+                        joueur2.gagnePoint();
+                        //Le joueur 2 gagne les cartes
+                        while(!deckTempo.estVide())
+                            joueur2.ajouterCarte(deckTempo.derniereCarte());
+                        bataille = false;
+                        break;
+                    default:
+                        if(!joueur1.deckVide())
+                            deckTempo.ajouterCarte(joueur1.tireCarte());
+                        if(!joueur2.deckVide())
+                            deckTempo.ajouterCarte(joueur2.tireCarte());
+                        bataille = true;
+                        break;
                 }
                 if(bataille)
                     System.out.println("Bataille !");
@@ -169,11 +143,69 @@ public class Bataille {
     }
 
     /**
-     * Demande au joueur ce qu'il veut faire et agit en consequence
+     * Affiche le message d'explication au debut et melange le deck de cartes
      */
-    public static void choixJoueur(Joueur joueur, Joueur autreJoueur) {
+    public static void msgInitJeuEtMelange(Deck deck) {
+        System.out.println("Jeu de la bataille!\n");
+        System.out.println("Le jeu se joue en tour durant lesquels les deux joueurs jouent leurs cartes en meme temps.");
+        System.out.println("Quand un des deux joueurs n'a plus de cartes ou se rend, le jeu s'arrete.");
+        deck.melange();
+    }
+    
+    /**
+     * Demande recursivement au joueur le mode de jeu qu'il veut
+     */
+    public static int choixModeJeu(int modeJeu) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("\nChoisissez votre mode de jeu parmis les 2 suivants :");
+        System.out.println(" 1. Deux joueurs\n 2. Contre IA\n 3. Mode rapide contre IA");
+        System.out.println("Votre choix : ");
+        modeJeu = scan.nextInt();
+        
+        if(modeJeu != 1 && modeJeu != 2 && modeJeu != 3)
+            modeJeu = choixModeJeu(modeJeu);
+        
+        return modeJeu;
+    }
+    
+    /**
+     * Récupère le pseudo des joueurs en passant par un menu et les attribue
+     */
+    public static void choixPseudoJoueur(Joueur joueur1, Joueur joueur2, int modeJeu) {
+        MenuPseudo menu = new MenuPseudo();
+        LecturePseudo lecturePseudo;
+        ArrayList<String> pseudos;
+        
+        lecturePseudo = menu.modeLecturePseudo();
+        if(modeJeu == 1)
+            pseudos = lecturePseudo.lirePseudo(false); //Sans IA
+        else
+            pseudos = lecturePseudo.lirePseudo(true); //Avec IA
+        attribuePseudo(joueur1, joueur2, pseudos);
+    }
+    
+    /**
+     * Distribue toute les cartes d'un deck aux deux joueurs de maniere egale
+     */
+    public static void distributionCartes(Joueur joueur1, Joueur joueur2, Deck deck) {
+        while(!deck.estVide()) {
+            if(deck.nbCartes()%2==0)
+                joueur1.ajouterCarte(deck.derniereCarte());
+            else
+                joueur2.ajouterCarte(deck.derniereCarte());
+        }
+    }
+    
+    /**
+     * Demande au joueur ce qu'il veut faire pour la suite et agit en consequence
+     */
+    public static void choixSuiteJoueur(Joueur joueur, Joueur autreJoueur) {
         Scanner scanChoix = new Scanner(System.in);
         String choix = "undefined";
+        
+        System.out.println("Chaque joueur peut maintenant quoi faire :");
+        System.out.println(" - entree : continuer\n - q+entree : abandonner\n - s+entree : afficher ses stats");
 
         while(!choix.equals("") && !choix.equals("q")) {
             System.out.println(joueur.afficheNom()+" : Que voulez-vous faire ? ");

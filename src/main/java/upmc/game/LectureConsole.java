8f8c4a8362bac421748d77de4c3a38b7c9fa2a76
Omar92ii <omar.abdallaa@gmail.com>
2017-10-13 16:17:16 +0200

    Modification de Lecture console

--- src/main/java/upmc/game/LectureConsole.java
+++ src/main/java/upmc/game/LectureConsole.java
@@ -11,7 +11,7 @@ public class LectureConsole implements LecturePseudo {
 
         Scanner sc = new Scanner(System.in);
 
-        int nbJoueur = 0;
+        int nbJoueur = 1;
 
         System.out.println("Veuillez entrer le nom du joueur " + nbJoueur);
 

8616423d18551fcbd4e6fb40a911ae36907618ea
abdalla <abdalla@134.157.46.183>
2017-10-13 11:49:19 +0200

    Modification de la classe Bataille, ajout du mode tour par tour

---
+++ src/main/java/upmc/game/MenuPseudo.java
@@ -0,0 +1,17 @@
+package upmc.game;
+
+import java.util.ArrayList;
+
+public class MenuPseudo {
+    
+    
+    
+    public LecturePseudo lirePseudo(){
+        
+        return null;
+        
+        
+        
+    }
+    
+}
